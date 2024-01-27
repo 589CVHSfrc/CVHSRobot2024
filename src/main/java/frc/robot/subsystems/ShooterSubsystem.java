a21bd84e6c3bb64b0ba6911060d057b11bd6ab6e
589CVHSfrc <CVHSfrc589@gmail.com>
2024-01-26 17:56:39 -0800

    PathPlanner Auto

--- src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -8,6 +8,7 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class ShooterSubsystem extends SubsystemBase {
   /** Creates a new ShooterSubsystem. */
+  
   public ShooterSubsystem() {}
 
   @Override
